7548bfa24f6cd0655a7e2bb78e4a0098a5d5e920
jaywonder20 <ogunyemijeremiah@gmail.com>
2020-05-12 19:12:09 +0100

    refractor flutter widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,62 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: new ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: new ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: new ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: new ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ),
+              Expanded(
+                child: new ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
